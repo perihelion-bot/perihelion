19caa4de48ef3a1e9f629c5b0d94ee0e7c6802a1
infernostars <infernity@infernity.dev>
2025-02-08 20:17:31 -0700

    more mapgame work

--- cfg/example.py
+++ cfg/example.py
@@ -16,8 +16,10 @@ EMBED = {"COLOR": 0xff8a8c, "FOOTER": f"Perihelion testing | v{VERSION}"} # This
 
 ERROR_LOGGING_CHANNEL = 1234567890123456789 # Errors will get logged to this channel.
 
-MAPGAME = {
-    "ENABLED": True, # Whether the mapgame system should be enabled.
+# Note that before you enable mapgame, you need to generate the map!
+# Run the mapgame cog in the root folder [e.g. the folder that contains assets, cfg, cogs, etc...]
+MAPGAME = { 
+    "ENABLED": False, # Whether the mapgame system should be enabled.
     "STEPRATE": 10, # How often ticks happen, in seconds. e.g. 10s/step
     "CHANNEL": 1234567890123456789 # Mapgame steps will be sent here.
 } 
--- cogs/90-ticking-events/mapgame.py
+++ cogs/90-ticking-events/mapgame.py
@@ -12,6 +12,11 @@ from cfg import VERSION, MAPGAME
 from PIL import Image
 from dataclasses import dataclass
 from enum import IntFlag, Enum
+import pickle
+from discord.ext import tasks
+import random
+
+import timeit
 
 @dataclass
 class Tile:
@@ -33,25 +38,29 @@ class Country:
         NEUTRAL = 0
         ENEMIES = -1
         WAR = -2
-        
-    relations = dict[int, Relations]
+    
+    name: str
+    relations: dict[int, Relations]
     id: int
     strength: int
-    balance: int
+    money: int
     income: int
 
 @dataclass
 class MapGameInstance:
     tiles: list[list[Tile]]
     border_tiles: list[Tile]
-    countries: list[Country]
+    _countries: list[Country]
+    turn: int
     
     @staticmethod
     def parse_image(img: Image):
         width, height = img.size
-
+        tiles = []
         for x in range(width):
-            for y in [48]:#range(height):
+            tiles.append([])
+            print(f"row {x+1}/{width} parsing")
+            for y in range(height):
                 _r, g, b, _a = img.getpixel((x, y))
                 match b:
                     case 255:
@@ -61,18 +70,114 @@ class MapGameInstance:
                     case 0:
                         flags = Tile.TileFlags.LAND | Tile.TileFlags.CROSSABLE
                 income = g >> 4
-                print(f"{x}, {y} | flags: {flags!r}, income: {income}")
+                tiles[x].append(Tile(flags, (x, y), income, 0))
+        return MapGameInstance(tiles, [], [], 0)
+    
+    class CountryAccessor: # Helper class to handle country access in a way that makes sense (e.g. not instance[2])
+        def __init__(self, countries_list: list[Country]):
+            self._countries_list = countries_list
 
+        def __getitem__(self, country_id: int) -> Country:
+            for country in self._countries_list:
+                if country.id == country_id:
+                    return country
+            raise KeyError(f"Country with id {country_id} not found.")
+
+        def __setitem__(self, country_id: int, country: Country):
+            if not isinstance(country, Country):
+                raise TypeError("Value must be a Country object.")
+            found = False
+            replaced_index = -1
+            for i, existing_country in enumerate(self._countries_list):
+                if existing_country.id == country_id:
+                    self._countries_list[i] = country
+                    found = True
+                    replaced_index = i
+                    break
+
+            if not found:
+                self._countries_list.append(country)
+                # Initialize relations for the new country and all existing countries
+                for c in self._countries_list:
+                    if c.id != country.id:
+                        if country.id not in c.relations:
+                            c.relations[country.id] = Country.Relations.NEUTRAL
+                        if c.id not in country.relations:
+                            country.relations[c.id] = Country.Relations.NEUTRAL
+
+
+        def __delitem__(self, country_id: int):
+            for i, country in enumerate(self._countries_list):
+                if country.id == country_id:
+                    del self._countries_list[i]
+                    return
+            raise KeyError(f"Country with id {country_id} not found.")
+        
+        def append(self, country: Country) -> int:
+            if not isinstance(country, Country):
+                raise TypeError("Value must be a Country object.")
+
+            existing_ids = {c.id for c in self._countries_list}
+            new_id = 1
+            while new_id in existing_ids:
+                new_id += 1
+            country.id = new_id
+
+            self._countries_list.append(country)
+
+            for c in self._countries_list:
+                if c.id != country.id:
+                    if country.id not in c.relations:
+                        c.relations[country.id] = Country.Relations.NEUTRAL
+                    if c.id not in country.relations:
+                        country.relations[c.id] = Country.Relations.NEUTRAL
+            return new_id
+
+    @property
+    def countries(self):
+        return MapGameInstance.CountryAccessor(self._countries)
+    
+    def random_available_tile(self) -> Tile | None:
+        """
+            WARNING: this function takes ~50ms! 
+        """
+        available_tiles = []
+        for row in self.tiles:
+            for tile in row:
+                if Tile.TileFlags.LAND | Tile.TileFlags.CROSSABLE in tile.flags and tile.owner_id == 0:
+                    available_tiles.append(tile)
+        if available_tiles:
+            return random.choice(available_tiles)
+        else:
+            return None
+    
+    def all_tiles_country(self, id: int) -> list[Tile] | None:
+        """
+            WARNING: this function takes ~50ms! 
+        """
+        available_tiles = []
+        for row in self.tiles:
+            for tile in row:
+                if tile.owner_id == id:
+                    available_tiles.append(tile)
+        return available_tiles
+    
 class MapGameCog(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.translator: JSONTranslator = client.tree.translator
+        self.channel = client.get_channel(MAPGAME["CHANNEL"])
 
     @commands.Cog.listener()
     async def on_ready(self):
         log.info("Cog: mapgame loaded")
     
-    def mapgame_step(self):
+    #@tasks.loop(seconds=MAPGAME["STEPRATE"])
+    async def mapgame_step_loop(self):
+        if MAPGAME["ENABLED"]:
+            await self.mapgame_step()
+    
+    async def mapgame_step(self):
         ...
         
     @app_commands.command(name="command_mapgame", description="command_mapgame")
@@ -97,6 +202,14 @@ if __name__ == "__main__":
         print(f"thats not an option")
     if num == "1":
         image = Image.open("assets/mapgame/map.png")
-        data = MapGameInstance.parse_image(image)
+        instance = MapGameInstance.parse_image(image)
+        cs1 = instance.random_available_tile()
+        cs2 = instance.random_available_tile()
+        id1 = instance.countries.append(Country("Test Country 1", {}, -1, 0, 0, 1))
+        id2 = instance.countries.append(Country("Test Country 2", {}, -1, 0, 0, 1))
+        cs1.owner_id = id1
+        cs2.owner_id = id2
+        #with open("data/mapgame.pickle", "wb") as f:
+        #    pickle.dump(instance, f)
     if num == "2":
         ...
